60ff82b097e943ccb7d9fb2edeb7943e0c1560f9
Thais Machado <machado.thais.06@gmail.com>
2025-07-07 14:00:11 -0300

    Atividade sistema bancario

---
+++ banco.py
@@ -0,0 +1,42 @@
+menu = """"
+[1] Depositar
+[2] Sacar
+[3] Extrato
+[0] Sair
+
+"""
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+while True:
+    opcao = input(menu)
+    if opcao == "1":
+        valor = float(input("Digite o valor a ser depositado: "))
+        if valor > 0:
+            saldo += valor
+            extrato += f"Depósito: R$ {valor:.2f}\n"
+            print("Depósito realizado com sucesso!")
+        else:
+            print("Valor inválido para depósito.")
+    elif opcao == "2":
+        valor = float(input("Digite o valor a ser sacado: "))
+        if numero_saques < LIMITE_SAQUES and valor <= saldo:
+            saldo -= valor
+            extrato += f"Saque: R$ {valor:.2f}\n"
+            numero_saques += 1
+            print("Saque realizado com sucesso!")
+        else:
+            print("Limite de saques atingido ou saldo insuficiente.")
+    elif opcao == "3":
+        print("=== Extrato ===")
+        print(extrato)
+        print(f"Saldo: R$ {saldo:.2f}")
+    elif opcao == "0":
+        print("Saindo...")
+        break
+    else:
+        print("Opção inválida.")
